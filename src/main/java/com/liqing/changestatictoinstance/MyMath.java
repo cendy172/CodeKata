b7bfd2be43aac6dea75f852deec8745ec15029e1
liqing <cendy172@gmail.com>
2014-03-23 17:27:46 +0800

    format change to instance method package

--- src/main/java/com/liqing/changestatictoinstance/MyMath.java
+++ src/main/java/com/liqing/changestatictoinstance/MyMath.java
@@ -1,11 +1,13 @@
 package com.liqing.changestatictoinstance;
 
-public class MyMath {
-    /*
-        1. change method signature with a MyMath instance
-        2. use convert to instance method action to change the method in usage place.
-    */
-    public int add(int a, int b) {
-        return a + b;
-    }
+public class MyMath
+{
+	/*
+	 * 1. change method signature with a MyMath instance 2. use convert to instance method action to change the method
+	 * in usage place.
+	 */
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
 }
