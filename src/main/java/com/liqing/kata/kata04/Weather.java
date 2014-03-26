855c952d9ae0e10cdf0f1ac1f9bb56b064cfe2c6
liqing <cendy172@gmail.com>
2014-03-26 19:43:34 +0800

    refactor: extract method to get weather records

--- src/main/java/com/liqing/kata/kata04/Weather.java
+++ src/main/java/com/liqing/kata/kata04/Weather.java
@@ -9,6 +9,14 @@ import org.apache.commons.io.IOUtils;
 
 public class Weather
 {
+
+	private List<WeatherRecord> weatherRecords;
+
+	public Weather()
+	{
+		weatherRecords = new ArrayList<WeatherRecord>();
+	}
+
 	public String getSmallestTemperatureSpreadDate()
 	{
 		String weather;
@@ -22,20 +30,12 @@ public class Weather
 			return "";
 		}
 
-		List<WeatherRecord> weatherRecords = new ArrayList<WeatherRecord>();
-
 		int minIndex = 0;
-		String[] rows = weather.split("\n");
+		String[] rows = getWeatherRecords(weather);
 
 		for (int i = 2; i < rows.length; i++)
 		{
-			String[] record = rows[i].trim().split("\\s+");
-
-			WeatherRecord weatherRecord = new WeatherRecord();
-			weatherRecord.setDate(record[0]);
-			weatherRecord.setMaxT(record[1]);
-			weatherRecord.setMinT(record[2]);
-			weatherRecords.add(weatherRecord);
+			WeatherRecord weatherRecord = getWeatherRecord(rows[i]);
 
 			if (weatherRecords.get(minIndex).getTemperatureSpread() > weatherRecord.getTemperatureSpread())
 			{
@@ -43,6 +43,23 @@ public class Weather
 			}
 		}
 		return weatherRecords.get(minIndex).getDate();
+	}
+
+	private WeatherRecord getWeatherRecord(String row)
+	{
+		String[] record = row.trim().split("\\s+");
+
+		WeatherRecord weatherRecord = new WeatherRecord();
+		weatherRecord.setDate(record[0]);
+		weatherRecord.setMaxT(record[1]);
+		weatherRecord.setMinT(record[2]);
+		weatherRecords.add(weatherRecord);
+		return weatherRecord;
+	}
+
+	private String[] getWeatherRecords(String weather)
+	{
+		return weather.split("\n");
 	}
 
 	private String getWeatherAsString(String source) throws IOException
