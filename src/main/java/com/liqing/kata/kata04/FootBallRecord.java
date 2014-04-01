8d8e7a87fa7d316b195f2c16a36b1636f8f5d854
liqing <cendy172@gmail.com>
2014-04-01 18:54:26 +0800

    add football record class

--- src/main/java/com/liqing/kata/kata04/FootBallRecord.java
+++ src/main/java/com/liqing/kata/kata04/FootBallRecord.java
@@ -1,5 +1,57 @@
 package com.liqing.kata.kata04;
 
-public class FootBallRecord {
+import static java.lang.Math.abs;
 
+public class FootBallRecord
+{
+
+	private String teamName;
+	private int scoredFor;
+	private int against;
+	private int difference;
+
+	public String getTeamName()
+	{
+		return teamName;
+	}
+
+	public void setTeamName(String teamName)
+	{
+		this.teamName = teamName;
+	}
+
+	public int getScoredFor()
+	{
+		return scoredFor;
+	}
+
+	public void setScoredFor(String scoredFor)
+	{
+		this.scoredFor = convertStringToInteger(scoredFor);
+	}
+
+	public int getAgainst()
+	{
+		return against;
+	}
+
+	public void setAgainst(String against)
+	{
+		this.against = convertStringToInteger(against);
+	}
+
+	public int getDifference()
+	{
+		return this.difference == 0 ? abs(scoredFor - against) : this.difference;
+	}
+
+	public void setDifference(int difference)
+	{
+		this.difference = difference;
+	}
+
+	private Integer convertStringToInteger(String temperature)
+	{
+		return Integer.parseInt(temperature.replace("*", ""));
+	}
 }
