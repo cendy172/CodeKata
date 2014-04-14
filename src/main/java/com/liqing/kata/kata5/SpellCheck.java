563aad72f17104c6fd9d531d74e807bac95c7ebf
liqing <cendy172@gmail.com>
2014-04-14 15:34:58 +0800

    add test for Spell checking

--- src/main/java/com/liqing/kata/kata5/SpellCheck.java
+++ src/main/java/com/liqing/kata/kata5/SpellCheck.java
@@ -2,4 +2,7 @@ package com.liqing.kata.kata5;
 
 public class SpellCheck {
 
+    public boolean checkSpell(String word) {
+        return false;
+    }
 }
---
+++ src/test/java/com/liqing/kata/kata5/SpellCheckTest.java
@@ -0,0 +1,35 @@
+package com.liqing.kata.kata5;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SpellCheckTest
+{
+
+	private SpellCheck spellCheck;
+
+	@Before
+	public void setUp()
+	{
+		spellCheck = new SpellCheck();
+	}
+
+	@Test
+	public void shouldBeCorrectWord()
+	{
+		boolean isCorrect = spellCheck.checkSpell("word");
+
+		assertThat(isCorrect, is(true));
+	}
+
+	@Test
+	public void shouldBeIncorrectWord()
+	{
+		boolean isCorrect = spellCheck.checkSpell("2");
+
+		assertThat(isCorrect, is(false));
+	}
+}
