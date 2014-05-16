f9b14298ec234a50025f7280932973b923335f11
liqing <cendy172@gmail.com>
2014-05-16 14:01:04 +0800

    format

--- src/main/java/com/liqing/kata/kata09/CheckOut.java
+++ src/main/java/com/liqing/kata/kata09/CheckOut.java
@@ -13,16 +13,15 @@ public class CheckOut {
 
     public int price(String items) {
         int total = 0;
-        this.items = new ArrayList<GoodType>();
         initItems(items);
         for (GoodType item : this.items) {
-
             total += priceRule.getGoods(item).getPrice();
         }
         return total;
     }
 
     private void initItems(String items) {
+        this.items = new ArrayList<GoodType>();
         for (char item : items.toCharArray()) {
             this.items.add(GoodType.valueOf(String.valueOf(item)));
         }
