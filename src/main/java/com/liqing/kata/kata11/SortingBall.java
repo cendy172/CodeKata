2b3249b8d7f7a12af651a7b53ab15dab43227753
liqing <cendy172@gmail.com>
2014-05-20 21:24:01 +0800

    add unit test.

--- src/main/java/com/liqing/kata/kata11/SortingBall.java
+++ src/main/java/com/liqing/kata/kata11/SortingBall.java
@@ -1,5 +1,19 @@
 package com.liqing.kata.kata11;
 
+import java.util.List;
+
 public class SortingBall {
 
+    public void add(int number) {
+
+    }
+
+    public List<Integer> balls() {
+        return null;
+    }
+
+    public void newRound() {
+
+
+    }
 }
--- src/test/java/com/liqing/kata/kata11/SortingBallTest.java
+++ src/test/java/com/liqing/kata/kata11/SortingBallTest.java
@@ -1,5 +1,30 @@
 package com.liqing.kata.kata11;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class SortingBallTest {
 
+    private SortingBall sortingBall;
+
+    @Before
+    public void setUp() {
+        sortingBall = new SortingBall();
+    }
+
+    @Test
+    public void shouldAddSorted() {
+        sortingBall.newRound();
+        sortingBall.add(20);
+
+        List<Integer> balls = sortingBall.balls();
+
+        assertThat(balls.size(), is(1));
+        assertThat(balls.get(0), is(20));
+    }
 }
